bea0aa061d31e3f0092c0130a8ceb6e3d6d49a5e
bamirade <118993260+bamirade@users.noreply.github.com>
2023-10-14 11:44:25 +0800

    Rating Show

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,26 @@
 import { useEffect, useState } from "react";
 import Snackbar from "../utils/snackbar";
 import { PageIndicator } from "../utils/pageindicator";
+import axios from "axios";
+import key from "../api/key";
+interface Commission {
+  commission_id: number;
+  buyer_username?: string;
+  seller_username?: string;
+  title: string;
+  price: string;
+  stage: string;
+  description: string;
+  rating: number | null;
+}
 
 function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
   const [snackbarOpen, setSnackbarOpen] = useState<boolean>(false);
   const [snackbarMessage, setSnackbarMessage] = useState<string | null>(null);
+  const [type, setType] = useState<boolean>(true);
+  const [commissionCount, setCommissionCount] = useState(0);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -14,12 +28,44 @@ function Header() {
 
   useEffect(() => {
     const token = localStorage.getItem("token");
+    const type = localStorage.getItem("type");
 
     if (token) {
       setIsLoggedIn(true);
     } else {
       setIsLoggedIn(false);
     }
+
+    if (type === "Buyer") {
+      setType(true);
+    } else if (type === "Seller") {
+      setType(false);
+    }
+
+    const fetchData = async () => {
+      try {
+        const userType = localStorage.getItem("type");
+        const authToken = localStorage.getItem("token");
+        const apiUrl = `${key.API_URL}/commission/${userType?.toLowerCase()}`;
+
+        const response = await axios.get(apiUrl, {
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+          },
+        });
+
+        const commissions = response.data.commissions;
+        const nonCompletedCommissions = commissions.filter(
+          (commission: Commission) => commission.stage !== "Completed"
+        );
+
+        setCommissionCount(nonCompletedCommissions.length);
+      } catch (error) {
+        console.error("Error fetching commission data:", error);
+      }
+    };
+
+    fetchData();
   }, []);
 
   const handleLogout = () => {
@@ -116,33 +162,34 @@ function Header() {
                 Login
               </a>
             )}
-
-            <a
-              href="/cart"
-              className="text-black hover:text-gray-600 transition duration-300"
-            >
-              <div className="relative py-2">
-                <div className="t-0 absolute left-3">
-                  <p className="flex h-1 w-1 items-center justify-center rounded-full bg-red-500 p-2 text-xs text-white">
-                    1
-                  </p>
+            {type && (
+              <a
+                href="/commissions"
+                className="text-black hover:text-gray-600 transition duration-300"
+              >
+                <div className="relative py-2">
+                  <div className="t-0 absolute left-3">
+                    <p className="flex h-1 w-1 items-center justify-center rounded-full bg-red-500 p-2 text-xs text-white">
+                      {commissionCount}
+                    </p>
+                  </div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="file: h-6 w-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+                    />
+                  </svg>
                 </div>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="file: h-6 w-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
-                  />
-                </svg>
-              </div>
-            </a>
+              </a>
+            )}
           </div>
         </div>
         <div className="flex justify-end text-xs mr-2 lg:show mb:hidden">
--- src/pages/ArtistProfile.tsx
+++ src/pages/ArtistProfile.tsx
@@ -98,6 +98,11 @@ const ArtistProfile: React.FC = () => {
     }
   };
 
+  const createShareURL = () => {
+    const shareLink = `https://www.facebook.com/sharer/sharer.php?u=https://project-camay-pgcd.onrender.com/artists/${username}`;
+    return encodeURI(shareLink);
+  };
+
   return (
     <div className="mx-auto">
       {artistData ? (
@@ -135,10 +140,153 @@ const ArtistProfile: React.FC = () => {
               {artistData.bio || "Bio is currently empty"}
             </p>
             <p className="mt-2 text-black">
-              {artistData.rating !== 0
-                ? `Rating: ${artistData.rating}`
-                : "Rating is currently unavailable"}
+              {artistData.rating !== 0 ? (
+                <ul
+                  className="my-1 flex list-none gap-1 p-0 w-full justify-center"
+                  data-te-rating-init
+                >
+                  <li>
+                    <span
+                      className="text-primary [&>svg]:h-5 [&>svg]:w-5"
+                      title="Bad"
+                      data-te-rating-icon-ref
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill={`${artistData.rating >= 1 ? "yellow" : "none"}`}
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+                        />
+                      </svg>
+                    </span>
+                  </li>
+                  <li>
+                    <span
+                      className="text-primary [&>svg]:h-5 [&>svg]:w-5"
+                      title="Poor"
+                      data-te-rating-icon-ref
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill={`${artistData.rating >= 2 ? "yellow" : "none"}`}
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+                        />
+                      </svg>
+                    </span>
+                  </li>
+                  <li>
+                    <span
+                      className="text-primary [&>svg]:h-5 [&>svg]:w-5"
+                      title="OK"
+                      data-te-rating-icon-ref
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill={`${artistData.rating >= 3 ? "yellow" : "none"}`}
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+                        />
+                      </svg>
+                    </span>
+                  </li>
+                  <li>
+                    <span
+                      className="text-primary [&>svg]:h-5 [&>svg]:w-5"
+                      title="Good"
+                      data-te-rating-icon-ref
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill={`${artistData.rating >= 4 ? "yellow" : "none"}`}
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+                        />
+                      </svg>
+                    </span>
+                  </li>
+                  <li>
+                    <span
+                      className="text-primary [&>svg]:h-5 [&>svg]:w-5"
+                      title="Excellent"
+                      data-te-rating-icon-ref
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill={`${artistData.rating >= 5 ? "yellow" : "none"}`}
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+                        />
+                      </svg>
+                    </span>
+                  </li>
+                </ul>
+              ) : (
+                "Rating is currently unavailable"
+              )}
             </p>
+            <div id="fb-root"></div>
+            <script
+              async
+              defer
+              crossOrigin="anonymous"
+              src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v18.0"
+              nonce="g1xF8ZXR"
+            ></script>
+            <div
+              className="fb-share-button flex justify-center"
+              data-href={`https://project-camay-pgcd.onrender.com/artists/${username}`}
+              data-layout="button"
+
+            >
+              <a
+                target="_blank"
+                href={createShareURL()}
+                className="fb-xfbml-parse-ignore"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  x="0px"
+                  y="0px"
+                  width="30"
+                  height="30"
+                  viewBox="0 0 30 30"
+                  fill="blue"
+                >
+                  <path d="M15,3C8.373,3,3,8.373,3,15c0,6.016,4.432,10.984,10.206,11.852V18.18h-2.969v-3.154h2.969v-2.099c0-3.475,1.693-5,4.581-5 c1.383,0,2.115,0.103,2.461,0.149v2.753h-1.97c-1.226,0-1.654,1.163-1.654,2.473v1.724h3.593L19.73,18.18h-3.106v8.697 C22.481,26.083,27,21.075,27,15C27,8.373,21.627,3,15,3z"></path>
+                </svg>
+              </a>
+            </div>
           </div>
           <div className="mt-4 text-center">
             <button
